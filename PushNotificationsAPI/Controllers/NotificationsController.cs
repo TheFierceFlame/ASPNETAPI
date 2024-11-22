42ce6c1f46797db87c150d6a7d850aabd3b751a7
TheFierceFlame <fierceflamemusic@gmail.com>
2024-11-21 19:26:44 -0700

    Inicializacion del proyecto

---
+++ PushNotificationsAPI/Controllers/NotificationsController.cs
@@ -0,0 +1,35 @@
+﻿using PushNotificationsAPI.DTO;
+using PushNotificationsAPI.Manager;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace PushNotificationsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly FireBaseNotificationsRepositoryInterface FirebaseNotificationsRepository;
+
+        public NotificationsController(FireBaseNotificationsRepositoryInterface NotificationsRepository)
+        {
+            FirebaseNotificationsRepository = NotificationsRepository;
+        }
+
+        [HttpPost("/Enviar")]
+        public async Task<IActionResult> Send(MessageDTO Message)
+        {
+            bool Result = await FirebaseNotificationsRepository.SendPushNotification(Message);
+
+            return Result ? Ok(Message) : BadRequest();
+        }
+
+        [HttpPost("/EnviarGeneral")]
+        public async Task<IActionResult> SendAll(MessageDTO Message)
+        {
+            bool Result = await FirebaseNotificationsRepository.SendGeneralPushNotification(Message);
+
+            return Result ? Ok(Message) : BadRequest();
+        }
+    }
+}
---
+++ PushNotificationsAPI/Manager/FireBaseNotificationsRepositoryInterface.cs
@@ -0,0 +1,11 @@
+﻿using PushNotificationsAPI.DTO;
+using System.Threading.Tasks;
+
+namespace PushNotificationsAPI.Manager
+{
+    public interface FireBaseNotificationsRepositoryInterface
+    {
+        public Task<bool> SendPushNotification(MessageDTO Message);
+        public Task<bool> SendGeneralPushNotification(MessageDTO Message);
+    }
+}
